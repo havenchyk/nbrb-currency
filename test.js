be5bb4ad1ac92b6b0b401e3782e7df6140fe818d
Uladzimir Havenchyk <uladzimir_havenchyk@epam.com>
2015-05-12 10:23:21 +0300

    Exclude npm log, updated tests.

--- test.js
+++ test.js
@@ -2,36 +2,38 @@ import currency from './index.es6';
 import moment from 'moment';
 import {expect} from 'chai';
 
-it('Should download currency for today', (done) => {
-    currency((err, data) => {
-        expect(err).to.be.a('null');
-        expect(data).to.be.an('object');
+describe('Check download functionality', function () {
+    it('Should download currency for today', (done) => {
+        currency((err, data) => {
+            expect(err).to.be.a('null');
+            expect(data).to.be.an('object');
 
-        expect(data.date).to.be.a('string');
-        expect(data.date).to.be.equal(moment().format('MM/DD/YYYY'));
+            expect(data.date).to.be.a('string');
+            expect(data.date).to.be.equal(moment().format('MM/DD/YYYY'));
 
-        expect(data.currencies).to.be.an('array');
-        expect(data.currencies.find((item) => {
-            return item.charCode === 'USD';
-        })).to.be.an('object');
+            expect(data.currencies).to.be.an('array');
+            expect(data.currencies.find((item) => {
+                return item.charCode === 'USD';
+            })).to.be.an('object');
 
-        done();
+            done();
+        });
     });
-});
 
-it('Should download currency for another day', (done) => {
-    currency('12/24/2014', (err, data) => {
-        expect(err).to.be.a('null');
-        expect(data).to.be.an('object');
+    it('Should download currency for 12/24/2014', (done) => {
+        currency('12/24/2014', (err, data) => {
+            expect(err).to.be.a('null');
+            expect(data).to.be.an('object');
 
-        expect(data.date).to.be.a('string');
-        expect(data.date).to.be.equal('12/24/2014');
+            expect(data.date).to.be.a('string');
+            expect(data.date).to.be.equal('12/24/2014');
 
-        expect(data.currencies).to.be.an('array');
-        expect(data.currencies.find((item) => {
-            return item.charCode === 'USD';
-        }).rate).to.be.equal('10950');
+            expect(data.currencies).to.be.an('array');
+            expect(data.currencies.find((item) => {
+                return item.charCode === 'USD';
+            }).rate).to.be.equal('10950');
 
-        done();
+            done();
+        });
     });
 });
