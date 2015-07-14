84c456a9f19d02f6c51c301e84446742a7917a27
Uladzimir Havenchyk <uladzimir_havenchyk@epam.com>
2015-07-14 13:14:47 +0300

    Use got instead of http. Closes #1

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-import download from './src/downloadXML';
+import got from 'got';
 import moment from 'moment';
 import fs from 'fs';
 import {parseString} from 'xml2js';
 
 const currentDate = moment().format('MM/DD/YYYY'),
-    endPoint = 'http://www.nbrb.by/Services/XmlExRates.aspx',
-    temporaryFileName = 'currency.xml',
-    encoding = 'utf-8';
+      endPoint = 'http://www.nbrb.by/Services/XmlExRates.aspx',
+      temporaryFileName = 'currency.xml';
 
 export default (date, cb) => {
     if (typeof date === 'function') {
@@ -14,8 +13,15 @@ export default (date, cb) => {
         date = currentDate;
     }
 
-    download(endPoint + '?ondate=' + date, function (xml) {
-        parseString(xml, function (err, jsonData) {
+    let url = endPoint + '?ondate=' + date;
+
+    got(url, function (err, data) {
+        if (err) {
+            cb(err);
+            return;
+        }
+
+        parseString(data, function (err, jsonData) {
             if (err) {
                 cb(err);
                 return;
