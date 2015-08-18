008a0397e7da86925f6fc24e720c3f4f4f73ff5e
Uladzimir Havenchyk <uladzimir_havenchyk@epam.com>
2015-08-18 13:52:42 +0300

    Promises and callbacks now available

--- index.js
+++ index.js
@@ -7,42 +7,90 @@ const currentDate = moment().format('MM/DD/YYYY'),
       endPoint = 'http://www.nbrb.by/Services/XmlExRates.aspx',
       temporaryFileName = 'currency.xml';
 
-export default (date, cb) => {
-    if (typeof date === 'function') {
-        cb = date;
-        date = currentDate;
-    } else if (!date) {
-      date = currentDate;
+
+
+function normalizeArguments(opts) {
+    if (!opts) {
+        opts = {};
+        opts.date = currentDate;
+    } else if (typeof opts === 'string') {
+        const date = opts;
+        opts = {};
+        opts.date = date;
     }
 
-    const url = `${endPoint}?ondate=${date}`;
+    if (!opts.date) {
+        opts.date = currentDate;
+    }
 
-    return got.get(url).then(res => {
-          const xml = res.body;
+    opts.url = `${endPoint}?ondate=${opts.date}`;
 
-          return new Promise(function(resolve) {
-            parseString(xml, (err, jsonData) => {
-                    if (err) {
-                        return;
-                    }
+    return opts;
+}
 
-                    const day = jsonData.DailyExRates['$'].Date;
-                    const currencies = jsonData.DailyExRates['Currency'].map(item => {
-                        return {
-                            charCode: item.CharCode[0],
-                            name: item.Name[0],
-                            rate: item.Rate[0]
-                        };
-                    });
+function asCallback(opts, cb) {
+    got.get(opts.url, function(err, xml) {
+        if (err) {
+            cb(err);
+            return;
+        }
 
-                    const dateForDist = {
-                        date: day,
-                        currencies: currencies
-                    };
+        parseString(xml, (err, data) => {
+            if (err) {
+                cb(err);
+                return;
+            }
 
-                    resolve(dateForDist);
-                });
-          })
-      })
-      .catch(console.error);
-};
+            cb(null, normalizeResponse(data))
+        });
+    });
+}
+
+function asPromise(opts) {
+  return new Promise(function (resolve, reject) {
+		asCallback(opts, function (err, data) {
+			if (err) {
+				reject(err);
+				return;
+			}
+
+			resolve(data);
+		});
+	});
+}
+
+function download(opts, cb) {
+	if (typeof opts === 'function') {
+		cb = opts;
+		opts = {};
+	}
+
+	opts = normalizeArguments(opts);
+
+	if (cb) {
+		asCallback(opts, cb);
+		return;
+	}
+
+	return asPromise(opts);
+}
+
+function normalizeResponse(jsonData) {
+    const day = jsonData.DailyExRates['$'].Date;
+    const currencies = jsonData.DailyExRates['Currency'].map(item => {
+        return {
+            charCode: item.CharCode[0],
+            name: item.Name[0],
+            rate: item.Rate[0]
+        };
+    });
+
+    const dateForDist = {
+        date: day,
+        currencies: currencies
+    };
+
+    return dateForDist;
+}
+
+export default download;
--- test.js
+++ test.js
@@ -7,9 +7,8 @@ describe('Check download functionality', function () {
         expect(currency).to.be.a('function');
     });
 
-    it('Should download currency for today', (done) => {
+    it('Should download currency for current date as promise', (done) => {
         currency().then(data => {
-          console.log('then!!!');
             expect(data).to.be.an('object');
             expect(data.date).to.be.a('string');
             expect(data.date).to.be.equal(moment().format('MM/DD/YYYY'));
@@ -23,8 +22,42 @@ describe('Check download functionality', function () {
         });
     });
 
-    it('Should download currency for 12/24/2014', (done) => {
+    it('Should download currency for current date as callback', (done) => {
+        currency((err, data) => {
+            expect(err).to.be.a('null');
+
+            expect(data).to.be.an('object');
+            expect(data.date).to.be.a('string');
+            expect(data.date).to.be.equal(moment().format('MM/DD/YYYY'));
+
+            expect(data.currencies).to.be.an('array');
+            expect(data.currencies.find((item) => {
+                return item.charCode === 'USD';
+            })).to.be.an('object');
+
+            done();
+        });
+    });
+
+    it('Should download currency for 12/24/2014 as promise', (done) => {
         currency('12/24/2014').then(data => {
+            expect(data).to.be.an('object');
+
+            expect(data.date).to.be.a('string');
+            expect(data.date).to.be.equal('12/24/2014');
+
+            expect(data.currencies).to.be.an('array');
+            expect(data.currencies.find((item) => {
+                return item.charCode === 'USD';
+            }).rate).to.be.equal('10950');
+
+            done();
+        });
+    });
+
+    it('Should download currency for 12/24/2014 as callback', (done) => {
+        currency('12/24/2014', (err, data) => {
+            expect(err).to.be.a('null');
             expect(data).to.be.an('object');
 
             expect(data.date).to.be.a('string');
