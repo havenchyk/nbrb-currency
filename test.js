4fc45f88ba7d287f1e1324c96c3604a628f1e805
havenchyk <havenchyk@gmail.com>
2015-12-16 21:27:26 +0300

    Fixed tests

--- test.js
+++ test.js
@@ -1,20 +1,22 @@
 import currency from './index';
-import moment from 'moment';
 import {expect} from 'chai';
+
+const today = new Date();
+const date = [today.getMonth() + 1, today.getDate(), today.getFullYear()].join('/');
 
 describe('Check download functionality', function () {
     it('Currency object should be available', () => {
         expect(currency).to.be.a('function');
     });
 
-    it('Should download currency for current date as promise', (done) => {
+    it('Should download currency for current date as promise', done => {
         currency().then(data => {
             expect(data).to.be.an('object');
             expect(data.date).to.be.a('string');
-            expect(data.date).to.be.equal(moment().format('MM/DD/YYYY'));
+            expect(data.date).to.be.equal(date);
 
             expect(data.currencies).to.be.an('array');
-            expect(data.currencies.find((item) => {
+            expect(data.currencies.find(item => {
                 return item.charCode === 'USD';
             })).to.be.an('object');
 
@@ -22,16 +24,16 @@ describe('Check download functionality', function () {
         });
     });
 
-    it('Should download currency for current date as callback', (done) => {
+    it('Should download currency for current date as callback', done => {
         currency((err, data) => {
             expect(err).to.be.a('null');
 
             expect(data).to.be.an('object');
             expect(data.date).to.be.a('string');
-            expect(data.date).to.be.equal(moment().format('MM/DD/YYYY'));
+            expect(data.date).to.be.equal(date);
 
             expect(data.currencies).to.be.an('array');
-            expect(data.currencies.find((item) => {
+            expect(data.currencies.find(item => {
                 return item.charCode === 'USD';
             })).to.be.an('object');
 
@@ -39,7 +41,7 @@ describe('Check download functionality', function () {
         });
     });
 
-    it('Should download currency for 12/24/2014 as promise', (done) => {
+    it('Should download currency for 12/24/2014 as promise', done => {
         currency('12/24/2014').then(data => {
             expect(data).to.be.an('object');
 
@@ -47,7 +49,7 @@ describe('Check download functionality', function () {
             expect(data.date).to.be.equal('12/24/2014');
 
             expect(data.currencies).to.be.an('array');
-            expect(data.currencies.find((item) => {
+            expect(data.currencies.find(item => {
                 return item.charCode === 'USD';
             }).rate).to.be.equal('10950');
 
@@ -55,7 +57,7 @@ describe('Check download functionality', function () {
         });
     });
 
-    it('Should download currency for 12/24/2014 as callback', (done) => {
+    it('Should download currency for 12/24/2014 as callback', done => {
         currency('12/24/2014', (err, data) => {
             expect(err).to.be.a('null');
             expect(data).to.be.an('object');
@@ -64,7 +66,7 @@ describe('Check download functionality', function () {
             expect(data.date).to.be.equal('12/24/2014');
 
             expect(data.currencies).to.be.an('array');
-            expect(data.currencies.find((item) => {
+            expect(data.currencies.find(item => {
                 return item.charCode === 'USD';
             }).rate).to.be.equal('10950');
 
